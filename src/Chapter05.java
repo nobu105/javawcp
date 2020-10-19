1448d362a3377d3d05db155a56ca925d1b02f9e4
岩井　將輝 <iwai@192.168.0.15>
2020-10-19 16:36:15 +0900

    ５章終わり

---
+++ src/Chapter05.java
@@ -0,0 +1,21 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 11L;
+		System.out.println(long1);
+		double double1 = 1.11d;
+		System.out.println(double1);
+		boolean boolean1 = true;
+		System.out.println(boolean1);
+		
+		Float float1 = 0.11f;
+		System.out.println(float1);
+		Integer integer1 = 123;
+		System.out.println(integer1);
+		Character character1 =  'あ';
+		System.out.println(character1);
+		
+		int[] array1 = { 0, 1, 2, 3, 4 };
+		System.out.println(array1[3]);
+	}
+}
