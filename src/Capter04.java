1199d5d1908ee567980a9b5966151021a9b5d822
岩井　將輝 <iwai@192.168.0.15>
2020-10-19 14:21:22 +0900

    first commit

---
+++ src/Capter04.java
@@ -0,0 +1,14 @@
+
+public class Capter04 {
+	
+	public static void main(String[] args) {
+		String str1 = "Nobu";
+		System.out.println(str1);
+		
+		str1 = "yeah";
+		System.out.println(str1);
+		
+		final int LUCKY_NUMBER = 5;
+		System.out.println(LUCKY_NUMBER);
+	}
+}
