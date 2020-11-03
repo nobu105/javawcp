1aecdfa99583586fe9446f212546518c29bbf727
nobu105 <dai9bensan@gmail.com>
2020-11-03 13:11:23 +0900

    8章終わり

--- src/Chapter08.java
+++ src/Chapter08.java
@@ -6,5 +6,15 @@ public class Chapter08 {
 			System.out.println(number * number);
 			number ++;
 		}
+		int[] array = { 1, 2, 3, 4 };
+		for (int i = 0; i  < array.length; i++) {
+			System.out.println(array[i]);
+			}
+		for (int num : array) {
+			if (num % 2 == 0) {
+				continue;
+			}
+			System.out.println(num);
+		}
 	}
 }
